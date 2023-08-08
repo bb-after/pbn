c576e821d190ab8bab663abc24c05299921b41fb
bb-after <117300088+bb-after@users.noreply.github.com>
2023-08-07 21:10:48 -0400

    added new files.

---
+++ pages/index.tsx
@@ -0,0 +1,54 @@
+// pages/index.tsx
+
+import React from 'react';
+import Form from '../components/Form';
+import Image from 'next/image';
+
+const Home: React.FC = () => {
+  return (
+    
+
+    <div style={{ padding: 16, margin: 'auto', maxWidth: 750, background: 'rgb(250 250 250)' }}>
+      <div style={{ background: '#000', padding: 3 }}>
+      <style jsx global>
+        {`
+          h1 {
+            font-family: 'Bungee Inline', sans-serif;
+            font-weight: 400;
+            font-size: 60px;
+            margin-top: 20px;
+          }
+        `}
+      </style>
+      <Image
+        priority
+        src="/images/sl-logo.png"
+        width={720}
+        height={80}	
+        style={{ objectFit: 'contain' }}
+        alt=""
+      />
+
+
+      </div>
+      
+      <div style={{ paddingTop: 20, display: 'flex' }}>
+
+        <Image
+        priority
+        src="/images/pbnj.png"
+        height={110}
+        width={110}
+        style={{ marginRight: 30 }}
+        alt=""
+      />
+
+        <h1>PBN'J</h1>
+      </div>
+      
+      <Form />
+    </div>
+  );
+};
+
+export default Home;
