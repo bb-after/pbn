faed923fa09d67481d4ec01c3e45f9fee6458aef
bb-after <117300088+bb-after@users.noreply.github.com>
2023-09-26 16:23:41 -0400

    Added pbn form view.

---
+++ pages/pbn-form.tsx
@@ -0,0 +1,25 @@
+// pages/pbn-form.tsx
+import React from 'react';
+import PbnSubmissionForm from '../components/PbnSubmissionForm'; // Adjust the import path
+import { EditorState, ContentState, convertFromHTML } from 'draft-js';
+
+const PbnFormPage: React.FC = () => {
+  // Define any initial values for the props if needed
+  const initialArticleTitle = 'Initial Article Title';
+  const initialEditorState = EditorState.createEmpty();
+
+  return (
+    <div>
+      <h1>Standalone PBN Form</h1>
+      <PbnSubmissionForm
+        onSubmit={(title, content) => {
+          // Handle form submission (e.g., post to PBN)
+        }}
+        articleTitle={initialArticleTitle}
+        pbnModalEditorState={initialEditorState}
+      />
+    </div>
+  );
+};
+
+export default PbnFormPage;
