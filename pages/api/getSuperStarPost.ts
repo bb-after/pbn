45b763b4e9ee5be15d3aae5d84849f83d435b09d
bb-after <117300088+bb-after@users.noreply.github.com>
2024-06-11 16:48:52 -0400

    big superstar changes, added edit, delete, view posts, and the scheduler to send out

---
+++ pages/api/getSuperStarPost.ts
@@ -0,0 +1,60 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import mysql from 'mysql2/promise';
+import { RowDataPacket } from 'mysql2';
+import axios from 'axios';
+import { getSlugFromUrl, findPostIdBySlug } from 'utils/urlUtils';
+
+// Your database connection options
+const dbConfig = {
+  host: process.env.DB_HOST_NAME,
+  user: process.env.DB_USER_NAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+};
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { id } = req.query;
+  try {
+  
+    const connection = await mysql.createConnection(dbConfig);
+    const [rows] = await connection.execute<RowDataPacket[]>('SELECT * FROM superstar_site_submissions join superstar_sites on superstar_site_submissions.superstar_site_id = superstar_sites.id WHERE superstar_site_submissions.id = ?', [id]);
+    await connection.end();
+
+    if (rows.length > 0) {
+      const submission = rows[0];
+      // Assuming 'wordpress_post_id' is the column that stores the WordPress post ID
+      const submission_response = submission.submission_response;
+      if (submission_response) {
+        const domain = submission.domain;
+        const slug = getSlugFromUrl(submission_response);  
+        const login = submission.login;
+        const password = submission.password;
+        const wordpressPostId = await findPostIdBySlug(domain, slug, { username: login, password: password });
+        const wordpressResponse = await axios.get(`${domain}/wp-json/wp/v2/posts/${wordpressPostId}`, {
+          auth: {
+            username: login,
+            password: password,
+          },
+        });
+        
+        const combinedData = {
+          pbn_site_id: submission.pbn_site_id,
+          title: submission.title,
+          client_name: submission.client_name,
+          content: wordpressResponse.data.content.rendered.replace(/(\n|<br\s*\/?>)/g, ' '),
+          categories: submission.categories,
+        };
+
+        return res.status(200).json(combinedData);
+      } else {
+        return res.status(404).json({ error: 'WordPress post ID not found for submission id ' + id });
+      }
+
+    } else {
+      return res.status(404).json({ error: 'Post not found for id' + id });
+    }
+  } catch (error) {
+    console.error('Database error:', error);
+    return res.status(500).json({ error: 'Failed to fetch post' });
+  }
+}
---
+++ pages/superstar-form.tsx
@@ -0,0 +1,45 @@
+// pages/superstar-form.tsx
+import React from "react";
+import SuperstarSubmissionForm from "../components/SuperstarSubmissionForm"; // Adjust the import path
+import { EditorState } from "draft-js";
+import { Typography, Link } from "@mui/material";
+
+const SuperstarFormPage: React.FC = () => {
+  const initialArticleTitle = "";
+  const initialEditorState = EditorState.createEmpty();
+
+  return (
+    <div>
+      <style jsx global>
+        {`
+          body {
+            background: #eee;
+          }
+        `}
+      </style>
+
+      <div
+        style={{
+          padding: "2rem",
+          borderRadius: "3px",
+          margin: "2rem",
+          background: "#fff",
+        }}
+      >
+        <Typography variant="h5" gutterBottom>
+          <Link href="https://sales.statuscrawl.io">Portal</Link> &raquo; Post
+          an Article to the Superstar
+        </Typography>
+        <SuperstarSubmissionForm
+          articleTitle={initialArticleTitle}
+          superstarModalEditorState={initialEditorState}
+          onSubmit={(title, content, tags) => {
+            console.log("Submitted:", title, content, tags);
+          }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SuperstarFormPage;
