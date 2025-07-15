44decb6edb49dfe626113892320e565c4e8bd04e
bb-after <117300088+bb-after@users.noreply.github.com>
2025-07-15 15:57:00 -0400

    more updates yo.

--- pages/api/validate-user-token.ts
+++ pages/api/validate-user-token.ts
@@ -1,13 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import mysql, { RowDataPacket } from 'mysql2/promise';
-
-// Database connection setup
-const dbConfig = {
-  host: process.env.DB_HOST_NAME,
-  user: process.env.DB_USER_NAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-};
+import { RowDataPacket } from 'mysql2/promise';
+import { query } from 'lib/db';
 
 // Function to validate user token
 export async function validateUserToken(req: NextApiRequest) {
@@ -18,29 +11,21 @@ export async function validateUserToken(req: NextApiRequest) {
   }
 
   try {
-    // Create a connection to the database
-    const connection = await mysql.createConnection(dbConfig);
+    // Query the database for the user token
+    const [rows]: [RowDataPacket[], any] = await query('SELECT * FROM users WHERE user_token = ?', [
+      token,
+    ]);
 
-    try {
-      // Query the database for the user token
-      const [rows]: [RowDataPacket[], any] = await connection.execute(
-        'SELECT * FROM users WHERE user_token = ?',
-        [token]
-      );
-
-      if (rows.length === 0) {
-        return { isValid: false, user_id: null, role: null };
-      }
-
-      return {
-        isValid: true,
-        user_id: rows[0].id,
-        username: rows[0].username,
-        role: rows[0].role || 'staff', // Default to 'staff' if role is not yet set
-      };
-    } finally {
-      await connection.end();
+    if (rows.length === 0) {
+      return { isValid: false, user_id: null, role: null };
     }
+
+    return {
+      isValid: true,
+      user_id: rows[0].id,
+      username: rows[0].username,
+      role: rows[0].role || 'staff', // Default to 'staff' if role is not yet set
+    };
   } catch (error) {
     console.error('Error validating user token:', error);
     return { isValid: false, user_id: null, role: null };
@@ -55,15 +40,11 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     return res.status(400).json({ error: 'Token is required' });
   }
 
-  // Create a connection to the database
-  const connection = await mysql.createConnection(dbConfig);
-
   try {
     // Query the database for the user token
-    const [rows]: [RowDataPacket[], any] = await connection.execute(
-      'SELECT * FROM users WHERE user_token = ?',
-      [token]
-    );
+    const [rows]: [RowDataPacket[], any] = await query('SELECT * FROM users WHERE user_token = ?', [
+      token,
+    ]);
 
     if (rows.length === 0) {
       return res.status(404).json({ valid: false });
@@ -83,7 +64,5 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   } catch (error) {
     console.error('Error validating user token:', error);
     res.status(500).json({ valid: false });
-  } finally {
-    await connection.end();
   }
 }
